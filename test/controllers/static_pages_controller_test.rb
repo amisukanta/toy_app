a18cea6fa528a10186c2ee84c75bcfbdd5a19992
Sukanta K Ghosh <amisukanta02@gmail.com>
2014-09-26 14:56:58 +0000

    Finished static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select 'title', 'Home | My Toy App'
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select 'title', 'Help | My Toy App'
+  end
+  
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select 'title', 'About | My Toy App'
   end
 
 end
