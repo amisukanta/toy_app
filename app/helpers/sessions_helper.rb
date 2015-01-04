a1005b79d734f0c248a4e3d35e2d4e6cb2c55afb
Sukanta K Ghosh <amisukanta02@gmail.com>
2015-01-04 00:32:49 +0000

    Finish crud actions with user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,10 @@ module SessionsHelper
     !current_user.nil?
   end
   
+  def current_user? (user)
+    user == current_user
+  end
+  
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -37,4 +41,14 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def redirect_to_or (default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+  
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,44 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
+  def setup
+    @user = users(:sukanta)
+    @another_user = users(:amrita)
+  end
+  
+  test "sould redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
   end
-
+  
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirected update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirect edit when not logged in as a wrong user" do
+    get :edit, id: @another_user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
+  test "should redirected update when not logged in as wrong user" do
+    patch :update, id: @another_user, user: { name: @another_user.name, email: @another_user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+  
 end
