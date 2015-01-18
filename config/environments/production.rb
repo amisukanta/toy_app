bd9d023a50d0ce1aad20b4fc051e83adf4241c2c
Sukanta K Ghosh <amisukanta02@gmail.com>
2015-01-18 22:58:01 +0000

    host name chnaged

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
    config.action_mailer.raise_delivery_errors = true
    config.action_mailer.delivery_method = :smtp
-   host = 'sukanta-toy.herokuapp.com'
+   host = '<sukanta-toy>.herokuapp.com'
    config.action_mailer.default_url_options = { host: host }
    ActionMailer::Base.smtp_settings = {
      :address             => 'smtp.sendgrid.net',
