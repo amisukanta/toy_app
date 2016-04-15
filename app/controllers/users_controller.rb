9a75cdf0ed6abb35f168bd7c191144b0cd450971
amisukanta <amisukanta02@gmail.com>
2016-04-15 10:26:01 +0200

    cleaning user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,35 +3,35 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
-  
+
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
   end
-  
+
   def show
     @microposts = @user.microposts.paginate(page: params[:page])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def edit
     #@user = User.find(params[:id])
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
       @user.send_activation_email
       #log_in @user
       flash[:info] = "Please check your email for account activation"
-      redirect_to root_url 
+      redirect_to root_url
     else
       render 'new'
     end
   end
-  
+
   def update
     if  @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
@@ -40,13 +40,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
   end
-  
+
   def following
     @title = "Following"
     @user = User.find_by( params[:id] )
@@ -60,16 +60,16 @@ class UsersController < ApplicationController
     render 'show_follow'
   end
   private
-  
+
     def set_user
       @user = User.find(params[:id])
     end
-    
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
-    #confirms a logged in user
+
+
     def logged_in_user
       unless logged_in?
         store_location
@@ -77,16 +77,15 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-    # returns correct user to edit and update user
+
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
     end
-    
-    # only admin users
+
+
     def admin_user
       redirect_to root_url unless current_user.admin?
     end
-    
-end
 
+end
